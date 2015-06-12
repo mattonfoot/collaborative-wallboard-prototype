6eca729532c84d3f1f499f11dfb8f1c65086ff5b
Matt Smith <mattonfoot@gmail.com>
2015-06-12 16:40:18 +0100

    updated app start for better error handling

--- dist/app.js
+++ dist/app.js
@@ -2035,6 +2035,8 @@ var $ = window.$
   , Queue = require('./queue')
   , UI = require('./ui')
   , Auth = require('./auth')
+  , RSVP = require('rsvp')
+  , Promise = RSVP.Promise
   , Application = require('./application');
 
 var $ = $ || function(){ console.log( 'jQuery not loaded.' ); };
@@ -2046,45 +2048,71 @@ var $dom = $('[data-provides="ui"]');
 var auth = new Auth( $dom, {} );
 auth.authenticate( 'X0n9ZaXJrJgeP9V4KAI7LXsiMsn6jN4G', 'vuu-se.auth0.com' )
   .then( onAuthenticate )
-  .catch( onError );
+  .catch( onAuthenticationError )
+  .then( onConfigure )
+  .catch( onConfigurationError )
+  .then( onReplicate )
+  .catch( onReplicationError );
+
+
+
+
+
 
 function onAuthenticate( profile ) {
   $dom.data( 'profile', profile );
 
+  return new Promise(function( resolve, reject ) {
   $.get( window.location.origin + '/config' )
     .done(function( config ) {
-      var channelName = config.channelName;
-      var remotedb = window.location.origin + config.dataURI + '/' + channelName;
-
-      var pouchdb = new PouchDB( channelName +'_'+ profile.user_id );
-
-      var queue = new Queue({
-        federate: true,
-        db: pouchdb,
-        channelName: channelName,
-        debug: true,
-        clientId: profile.user_id + Date.now()
-      });
-      $dom.data( 'queue', queue );
-
-      return queue.replicateFromRemote( remotedb ).then( onReplicate );
-
-      function onReplicate() {
-        var ui = new UI( queue, $dom, {}, $ );
-        $dom.data( 'ui', ui );
-
-        $dom.data( 'application', new Application( queue, ui, {} ) );
-
-        ui.enable();
-      }
+      resolve( config );
     });
+
+  });
 }
 
-function onError( err ) {
-  console.log( err );
+function onAuthenticationError( err ) {
+  console.log( 'AUTH ERROR', err );
 }
 
-},{"./application":1,"./auth":10,"./queue":12,"./ui":15,"pouchdb":158}],12:[function(require,module,exports){
+function onConfigure( config ) {
+  var profile = $dom.data( 'profile' );
+  var channelName = config.channelName;
+  var remotedb = window.location.origin + config.dataURI + '/' + channelName;
+
+  var pouchdb = new PouchDB( channelName +'_'+ profile.user_id );
+
+  var queue = new Queue({
+    federate: true,
+    db: pouchdb,
+    channelName: channelName,
+    debug: true,
+    clientId: profile.user_id + Date.now()
+  });
+  $dom.data( 'queue', queue );
+
+  return queue.replicateFromRemote( remotedb );
+}
+
+function onConfigurationError( err ) {
+  console.log( 'CONFIG ERROR', err );
+}
+
+function onReplicate() {
+  var queue = $dom.data( 'queue' );
+  var ui = new UI( queue, $dom, {}, $ );
+  $dom.data( 'ui', ui );
+
+  $dom.data( 'application', new Application( queue, ui, {} ) );
+
+  ui.enable();
+}
+
+function onReplicationError( err ) {
+  console.log( 'REPLICATION ERROR', err );
+}
+
+},{"./application":1,"./auth":10,"./queue":12,"./ui":15,"pouchdb":158,"rsvp":203}],12:[function(require,module,exports){
 var hrtime = require('performance-now');
 var postal = require('postal');
 
