fb6f393b427bc199f09e0ac935a2bbdd94969831
matt <mattonfoot@gmail.com>
2016-11-10 13:43:33 +0000

    minor code refactor for code styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,45 @@
-module.exports = function( grunt ) {
+const glob = require( 'glob' );
+
+const registerCustomGruntTasks = (grunt) => {
+  // clean
+  // grunt.registerTask('clean'     , [ 'clean' ]);
+
+  // test
+  grunt.registerTask('coverage'     , [ 'clean:coverage', 'blanket', 'copy:coverage', 'mochaTest:instrumented', 'mochaTest:lcov', 'mochaTest:coverage' ]);
+  grunt.registerTask('test'         , [ /*'jshint', 'eslint',*/ 'mochaTest:test' ]);
+
+  // build
+  grunt.registerTask('build'        , [ 'browserify', 'copy:client' ]);
+  grunt.registerTask('build:test'   , [ 'browserify:test' ]);
+  grunt.registerTask('rebuild'      , [ 'clean', 'build' ]);
+
+  // auto build
+  // grunt.registerTask('default'   , [ 'watch' ]);
+
+  // docker containers
+  grunt.registerTask('dock'         , [ 'dock:dev:build', 'dock:dev:start' ]);
+
+  // local dev servers
+  grunt.registerTask('serve'        , [ 'rebuild', 'nodemon:dev' ]);
+  grunt.registerTask('serve:local'  , [ 'rebuild', 'nodemon:local' ]);
+
+  // travis-ci
+  grunt.registerTask('ci'           , [ 'coverage', 'coveralls' ]);
+};
+
+// helper function to load task configs
+const loadConfig = (path, config) => {
+  var o = {};
+
+  glob.sync('*', { cwd: path }).forEach(function( option ) {
+    o[ option.replace( /\.js$/, '' ) ] = require( path + option )( config );
+  });
+
+  return o;
+};
+
+module.exports = (grunt) => {
     'use strict';
-
-    // helper function to load task configs
-
-    function loadConfig( path, config ) {
-        var glob = require( 'glob' );
-
-        var object = {}
-          , key;
-
-        glob.sync('*', { cwd: path })
-            .forEach(function( option ) {
-                key = option.replace( /\.js$/, '' );
-                object[key] = require( path + option )( config );
-            });
-
-        return object;
-    }
-
-    // actual config
 
     var config = {
         pkg: grunt.file.readJSON('package.json')
@@ -36,29 +57,5 @@ module.exports = function( grunt ) {
     // local tasks
     grunt.loadTasks('tasks');
 
-    // clean
-    // grunt.registerTask('clean'     , [ 'clean' ]);
-
-    // test
-    grunt.registerTask('coverage'     , [ 'clean:coverage', 'blanket', 'copy:coverage', 'mochaTest:instrumented', 'mochaTest:lcov', 'mochaTest:coverage' ]);
-    grunt.registerTask('test'         , [ /*'jshint', 'eslint',*/ 'mochaTest:test' ]);
-
-    // build
-    grunt.registerTask('build'        , [ 'browserify', 'copy:client' ]);
-    grunt.registerTask('build:test'   , [ 'browserify:test' ]);
-    grunt.registerTask('rebuild'      , [ 'clean', 'build' ]);
-
-    // auto build
-    // grunt.registerTask('default'   , [ 'watch' ]);
-
-    // docker containers
-    grunt.registerTask('dock'         , [ 'dock:dev:build', 'dock:dev:start' ]);
-
-    // local dev servers
-    grunt.registerTask('serve'        , [ 'rebuild', 'nodemon:dev' ]);
-    grunt.registerTask('serve:local'  , [ 'rebuild', 'nodemon:local' ]);
-
-    // travis-ci
-    grunt.registerTask('ci'           , [ 'coverage', 'coveralls' ]);
-
+    registerCustomGruntTasks(grunt);
 };
